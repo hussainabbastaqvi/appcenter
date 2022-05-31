6c21eee0b6e89a9eb05d7bae470835e9e05e5016
Hussain Abbas <13273598+hussainabbas@users.noreply.github.com>
2022-05-31 05:12:20 -0400

    appCenter

--- flutter_appcenter_bundle/flutter_base_project/lib/main.dart
+++ flutter_appcenter_bundle/flutter_base_project/lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_appcenter_bundle_example/config/app_config.dart';
-
-import 'package:package_info/package_info.dart';
 import 'package:flutter_appcenter_bundle/flutter_appcenter_bundle.dart';
+import 'package:flutter_appcenter_bundle_example/config/app_config.dart';
+import 'package:flutter_appcenter_bundle_example/helpers/utils/constants.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:package_info/package_info.dart';
+
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  var flavor = AppConfig(
+    flavorName: Constants.acceptanceFlavor,
+    baseUrl: Constants.acceptanceBaseUrl,
+    child: MyApp(),
+  );
+  await AppCenter.startAsync(
+    appSecretAndroid: 'd19ce942-2721-4d93-a26b-4d9cc7c04dd8',
+    appSecretIOS: 'd19ce942-2721-4d93-a26b-4d9cc7c04dd8',
+    enableDistribute: false,
+  );
+  await AppCenter.configureDistributeDebugAsync(enabled: false);
+  FlutterNativeSplash.remove();
+  return runApp(flavor);
+}
 
 class MyApp extends StatefulWidget {
   @override
